1d4c650acd6f2ab15c33e0f28cf4e92ea8ee1c4e
Unknown <juanrajc@hotmail.com>
2017-12-13 11:00:06 +0100

    ServicioHS hecho con fallos.

--- ServicioHS/app/src/main/java/com/htcdiurno/serviciohs/MainActivity.java
+++ ServicioHS/app/src/main/java/com/htcdiurno/serviciohs/MainActivity.java
@@ -1,13 +1,30 @@
 package com.htcdiurno.serviciohs;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Intent servicio=null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void iniciaServicio(View view){
+
+        startService(servicio=new Intent(this, MyService.class));
+
+    }
+
+    public void paraServicio(View view){
+
+        stopService(servicio);
+
+    }
+
 }
